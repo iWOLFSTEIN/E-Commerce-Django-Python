dda0296a3e4e05fe45364d4179bf0e16fa264ad2
Talha <mtalha7274@gmail.com>
2024-07-28 19:02:22 +0500

    user model created

---
+++ app_auth/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
+
+    REQUIRED_FIELDS = ["email", "password", "first_name", "last_name"]
+    USERNAME_FIELD = "email"
